a2ddc5895a9c329ff7600138f5d3e0846cc18f61
lzz <447089649@qq.com>
2018-11-26 17:34:50 +0800

    async和await简化代码

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -33,35 +33,56 @@ export default {
   },
   methods: {
     // 登录请求
-    handleLogin() {
-      this.$http.post('login', this.formdata).then(res => {
-        // console.log(res)
-        const {
-          data,
-          meta: { msg, status }
-        } = res.data
+    async handleLogin() {
+      // > 希望 让异步操作的代码 看起来像同步代码
+      // ES7 async+await
+      const res = await this.$http.post('login', this.formdata)
+      // console.log(res)
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data
 
-        if (status === 200) {
-          // 登录成功
-          // 1. 跳转home
-          this.$router.push({ name: 'home' })
-          // 2. 提示成功
-          this.$message.success(msg)
-        } else {
-          // 不成功
-          // 1. 提示消息
-          this.$message.warning(msg)
-        }
-        // // =>
-        // const {meta:{msg,status}} =
-        // {meta:{msg:"11",status:200}}
-        // // =>
-        // obj = {msg:"11",status:200}
-        // // =>
-        // {msg,status} = {msg:"11",status:200}
-        // // =>
-        // msg:11  status:200
-      })
+      if (status === 200) {
+        // 登录成功
+        // 1. 跳转home
+        this.$router.push({ name: 'home' })
+        // 2. 提示成功
+        this.$message.success(msg)
+      } else {
+        // 不成功
+        // 1. 提示消息
+        this.$message.warning(msg)
+      }
+
+      // this.$http.post('login', this.formdata).then(res => {
+      //   // console.log(res)
+      //   const {
+      //     data,
+      //     meta: { msg, status }
+      //   } = res.data
+
+      //   if (status === 200) {
+      //     // 登录成功
+      //     // 1. 跳转home
+      //     this.$router.push({ name: 'home' })
+      //     // 2. 提示成功
+      //     this.$message.success(msg)
+      //   } else {
+      //     // 不成功
+      //     // 1. 提示消息
+      //     this.$message.warning(msg)
+      //   }
+      // // =>
+      // const {meta:{msg,status}} =
+      // {meta:{msg:"11",status:200}}
+      // // =>
+      // obj = {msg:"11",status:200}
+      // // =>
+      // {msg,status} = {msg:"11",status:200}
+      // // =>
+      // msg:11  status:200
+      // })
     }
   }
 }
