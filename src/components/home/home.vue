3624e98e907b3fb2433124e47502624ab86441d8
lzz <447089649@qq.com>
2018-11-26 18:10:20 +0800

    头部样式调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,10 +1,27 @@
 <template>
 <el-container class="container">
-  <el-header class="header">Header</el-header>
-  <el-container>
-    <el-aside class="aside" width="200px">Aside</el-aside>
-    <el-main class="main">Main</el-main>
-  </el-container>
+    <el-header class="header">
+        <el-row>
+            <el-col :span="4">
+                <div class="grid-content bg-purple">
+                  <img src="../../assets/logo.png" alt="无法显示图片">
+                </div>
+            </el-col>
+            <el-col :span="18" class="middle">
+              <h3>电商后台管理系统</h3>
+            </el-col>
+            <el-col :span="2">
+                <div class="grid-content bg-purple">
+                  <a class="loginout" href="#">退出</a>
+                </div>
+            </el-col>
+        </el-row>
+
+    </el-header>
+    <el-container>
+        <el-aside class="aside" width="200px">Aside</el-aside>
+        <el-main class="main">Main</el-main>
+    </el-container>
 </el-container>
 </template>
 
@@ -16,13 +33,25 @@ export default {}
 .container {
   height: 100%;
 }
+
 .header {
   background-color: #b3c0d1;
 }
+
 .aside {
   background-color: #d3dce6;
 }
+
 .main {
   background-color: #e9eef3;
+}
+/* 头部样式 */
+.middle {
+  text-align: center;
+}
+.loginout {
+  line-height: 60px;
+
+  text-decoration: none;
 }
 </style>
