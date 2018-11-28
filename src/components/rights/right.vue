4f3ae7c70d8cb783005a5b85c27102124d07cd44
lzz <447089649@qq.com>
2018-11-28 18:09:46 +0800

    权限列表展示

--- src/components/rights/right.vue
+++ src/components/rights/right.vue
@@ -1,10 +1,30 @@
 <template>
 <el-card>
 
-  <!-- 1面包屑 -->
+    <!-- 1面包屑 -->
     <my-bread level1="权限管理" level2="权限列表"></my-bread>
 
-  <!-- 2表格 -->
+    <!-- 2表格 -->
+    <el-table
+    height="400px"
+    :data="rightlist" border style="width: 100%">
+        <el-table-column type="index" label="#" width="100">
+        </el-table-column>
+        <el-table-column prop="authName" label="权限名称" width="180">
+        </el-table-column>
+        <el-table-column prop="path" label="路径">
+        </el-table-column>
+
+        <el-table-column  label="层级">
+          <template slot-scope="scope">
+            <!-- scope.row.level 0 1 2  -->
+              <span v-if="scope.row.level==='0'">一级</span>
+              <span v-if="scope.row.level==='1'">二级</span>
+              <span v-if="scope.row.level==='2'">三级</span>
+          </template>
+        </el-table-column>
+    </el-table>
+
 </el-card>
 </template>
 
