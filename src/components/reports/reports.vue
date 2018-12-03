ed5a8503f3b5018c821ee57a09bdd88ca2f87ca6
lzz <447089649@qq.com>
2018-12-03 14:09:58 +0800

    调整笔记

---
+++ src/components/reports/reports.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <el-card>
+    <my-bread level1="数据统计" level2="数据报表"></my-bread>
+  </el-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  methods: {}
+};
+</script>
+
+<style>
+</style>
